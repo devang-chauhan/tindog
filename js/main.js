b19f384bff89f1df392ebf969b70567d7d502745
Devang <devngc@gmail.com>
2022-09-04 08:14:01 -0400

    refactor(*): functionality moved to update.js

--- js/main.js
+++ js/main.js
@@ -1,43 +1,15 @@
 import data from "./data.js";
-import Dog from "./dog.js";
+import Dog from "./Dog.js";
+import { updateDog, update } from "./update.js";
 
 const mainEl = document.getElementById("main");
 const disLikeEl = document.getElementById("dislike");
 const likeEl = document.getElementById("like");
 
-// an array of Dog objects
+
 const dogs = data.map(dog => new Dog(dog));
 
-// temporary variable
-let currentDog = '';
 
-
-function updateDog() {
-    currentDog = dogs.shift();
-    if (currentDog) {
-        mainEl.innerHTML = currentDog.getHTML();
-    } else {
-        mainEl.innerHTML = `
-            <div><p>No more dogs in your area.</p></div>
-        `;
-    }
-    
-}
-
-function updateStatus(choice) {
-    if (currentDog) {
-        currentDog.setMatchStatus(choice);
-    mainEl.innerHTML = currentDog.showStatus();
-    }
-}
-
-
-function run(choice) {
-    updateStatus(choice);
-    setTimeout(updateDog, 1000);    
-    
-}
-
-updateDog();
-likeEl.addEventListener("click", () => run(true));
-disLikeEl.addEventListener("click", () => run(false));
+updateDog(dogs, mainEl);
+likeEl.addEventListener("click", () => update(true, dogs, mainEl));
+disLikeEl.addEventListener("click", () => update(false, dogs, mainEl));
---
+++ js/update.js
@@ -0,0 +1,57 @@
+
+import Dog from './Dog.js';
+
+// a temporary variable that will be updated in following functions.
+let currentDog = '';
+
+
+/**
+ *Get a new Dog object from an array of Dog objects and get the HTML 
+ *representation of the Dog object.
+ *It the end of the array is reached and there's no Dog object, the function
+ *pushes a message as HTML.
+ * @param {Array[Dog]} dogArray - An array of Dog object. 
+ * @param {Element} el - Element from DOM to manipulate
+ */
+function updateDog(dogArray, el) {
+    currentDog = dogArray.shift();
+    if (currentDog) {
+        el.innerHTML = currentDog.getHTML();
+    } else {
+        el.innerHTML = `
+            <div><p>No more dogs in your area.</p></div>
+        `;
+    }
+}
+
+
+/**
+ * Update the HTML representation of a Dog object with the status of the Dog.
+ * It the end of the array is reached and there's no Dog object, the function
+ * will do nothing.
+ * @param {bool} choice - A boolean to indicate liked or disliked status of the Dog object. 
+ * @param {Dog} dog - A Dog object. 
+ * @param {Element} el - Element from DOM to manipulate
+ */
+function updateStatus(choice, dog, el) {
+    if (dog) {
+        dog.setMatchStatus(choice);
+    el.innerHTML = dog.showStatus();
+    }
+}
+
+
+/**
+ * This function adds status to the currently selected Dog on the screen and then updates the Dog on the screen.
+ * @param {bool} choice - A boolean to indicate liked or disliked status of the 
+ * @param {Array[Dog]} dogArray - An array of Dog object. 
+ * @param {Element} el - Element from DOM to manipulate
+ */
+function update(choice, dogs, el) {
+    updateStatus(choice, currentDog, el);
+    setTimeout(function () { updateDog(dogs, el); }, 1000);    
+    
+}
+
+
+export { updateDog, update };
